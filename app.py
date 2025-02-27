7a84f0ff0b823d635f35dd638106987dd3ba1272
abhinavnfr <abhinavrana18july@gmail.com>
2025-02-27 17:58:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import fetch_FASTA_sequence as fs
+# import fetch_FASTA_sequence as fs
 
 def main():
-    st.title("Protein Sequence Analysis Application")
+    st.title("Protein Sequence Analysis Workflow Application")
     st.write("")
     # fs.fetch_fasta_main()
 
